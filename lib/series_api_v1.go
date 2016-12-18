a359a62a181ca245eda4ed613326f988e87b764e
Nick Sweko <nicholas_swekosky@yahoo.com>
2016-12-17 23:43:26 -0600

    master Added methods for series data.

--- lib/series_api_v1.go
+++ lib/series_api_v1.go
@@ -1,5 +1,10 @@
 package lib
 
+import (
+	"encoding/json"
+	"errors"
+)
+
 type Seriess struct {
 	Start     string   `realtime_start`
 	End       string   `realtime_end`
@@ -22,4 +27,288 @@ type Series struct {
 	LastUpdated            string `last_updated`
 	Popularity             int    `popularity`
 	Notes                  string `notes`
+}
+
+type Observations struct {
+	Start        string        `realtime_start`
+	End          string        `realtime_end`
+	ObsStart     string        `observation_start`
+	ObsEnd       string        `observation_end`
+	Units        string        `units`
+	OutputType   int           `output_type`
+	FileType     string        `file_type`
+	OrderBy      string        `order_by`
+	SortOrder    string        `sort_order`
+	Count        int           `count`
+	Offset       int           `offset`
+	Limit        int           `limit`
+	Observations []Observation `observations`
+}
+
+type Observation struct {
+	Start string `realtime_start`
+	End   string `realtime_end`
+	Date  string `date`
+	Value string `value`
+}
+
+type VintageDates struct {
+	Start        string   `realtime_start`
+	End          string   `realtime_end`
+	OrderBy      string   `order_by`
+	SortOrder    string   `sort_order`
+	Count        int      `count`
+	Offset       int      `offset`
+	Limit        int      `limit`
+	VintageDates []string `vintage_dates`
+}
+
+func (f *FredClient) GetSeries(params map[string]interface{}) (*Seriess, error) {
+	if err := f.validateAPIKEY(); err != nil {
+
+		return nil, err
+
+	}
+
+	srs := &Seriess{}
+
+	resp, err := f.callAPI(params, "SERIES")
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(srs)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	return srs, nil
+
+}
+
+func (f *FredClient) GetSeriesCategories(params map[string]interface{}) (*Categories, error) {
+	if err := f.validateAPIKEY(); err != nil {
+
+		return nil, err
+
+	}
+
+	ctgs := &Categories{}
+
+	resp, err := f.callAPI(params, "SERIES_CATEGORIES")
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(ctgs)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	return ctgs, nil
+
+}
+
+func (f *FredClient) GetSeriesObservations(params map[string]interface{}) (*Observations, error) {
+	if err := f.validateAPIKEY(); err != nil {
+
+		return nil, err
+
+	}
+
+	obs := &Observations{}
+
+	resp, err := f.callAPI(params, "SERIES_OBSERVATIONS")
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(obs)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	return obs, nil
+
+}
+
+func (f *FredClient) GetSeriesRelease(params map[string]interface{}) (*Releases, error) {
+	if err := f.validateAPIKEY(); err != nil {
+
+		return nil, err
+
+	}
+
+	rls := &Releases{}
+
+	resp, err := f.callAPI(params, "SERIES_RELEASE")
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(rls)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	return rls, nil
+
+}
+
+func (f *FredClient) GetSeriesSearch(params map[string]interface{}) (*Seriess, error) {
+	if err := f.validateAPIKEY(); err != nil {
+
+		return nil, err
+
+	}
+
+	srs := &Seriess{}
+
+	resp, err := f.callAPI(params, "SERIES_SEARCH")
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(srs)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	return srs, nil
+
+}
+
+func (f *FredClient) GetSeriesSearchTags(params map[string]interface{}) (*Tags, error) {
+	if err := f.validateAPIKEY(); err != nil {
+
+		return nil, err
+
+	}
+
+	tags := &Tags{}
+
+	resp, err := f.callAPI(params, "SERIES_SEARCH_TAGS")
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(tags)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	return tags, nil
+
+}
+
+func (f *FredClient) GetSeriesSearchRelatedTags(params map[string]interface{}) (*Tags, error) {
+	if err := f.validateAPIKEY(); err != nil {
+
+		return nil, err
+
+	}
+
+	tags := &Tags{}
+
+	resp, err := f.callAPI(params, "SERIES_SEARCH_RELATED_TAGS")
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(tags)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	return tags, nil
+
+}
+
+func (f *FredClient) GetSeriesTags(params map[string]interface{}) (*Tags, error) {
+	if err := f.validateAPIKEY(); err != nil {
+
+		return nil, err
+
+	}
+
+	tags := &Tags{}
+
+	resp, err := f.callAPI(params, "SERIES_TAGS")
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(tags)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	return tags, nil
+
+}
+
+func (f *FredClient) GetSeriesUpdates(params map[string]interface{}) (*Seriess, error) {
+	if err := f.validateAPIKEY(); err != nil {
+
+		return nil, err
+
+	}
+
+	srs := &Seriess{}
+
+	resp, err := f.callAPI(params, "SERIES_UPDATES")
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(srs)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	return srs, nil
+
+}
+
+func (f *FredClient) GetSeriesVintageDates(params map[string]interface{}) (*VintageDates, error) {
+	if err := f.validateAPIKEY(); err != nil {
+
+		return nil, err
+
+	}
+
+	vds := &VintageDates{}
+
+	resp, err := f.callAPI(params, "SERIES_UPDATES")
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(vds)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	return vds, nil
+
 }
--- lib/tag_api_v1.go
+++ lib/tag_api_v1.go
@@ -8,5 +8,15 @@ type Tags struct {
 	Count     int      `count`
 	Offset    int      `offset`
 	Limit     int      `limit`
+	Tags      []Tag    `tag`
 	Sources   []Source `sources`
+}
+
+type Tag struct {
+	Name        string `name`
+	GroupId     string `group_id`
+	Notes       string `notes`
+	Created     string `created`
+	Popularity  int    `popularity`
+	SeriesCount int    `series_count`
 }
