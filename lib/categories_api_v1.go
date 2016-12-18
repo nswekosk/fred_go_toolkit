9951c628bed628590c4277f45ef242aed333c05e
Nick Sweko <nicholas_swekosky@yahoo.com>
2016-12-17 20:06:36 -0600

    master Added GetCategory method and updated CreateClient.

--- lib/categories_api_v1.go
+++ lib/categories_api_v1.go
@@ -1,7 +1,44 @@
 package lib
 
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
 type Category struct {
 	id        int    `id`
 	name      string `name`
 	parent_id string `parent_id`
+}
+
+func (f *FredClient) GetCategory(CategoryID string) (*Category, error) {
+
+	if err := f.validateAPIKEY(); err != nil {
+
+		return nil, err
+
+	}
+
+	if CategoryID == "" {
+		return nil, errors.New("You must provide a cateory id to retrieve a category. CategoryID's can be found at ")
+	}
+
+	cat := &Category{}
+	url := f.requestUrl + "/category_id?=" + CategoryID
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(cat)
+
+	if err != nil {
+		return nil, errors.New("There was an error in processing the query. Please contact the client administrator.")
+	}
+
+	return cat, nil
+
 }
--- lib/fred_api_v1.go
+++ lib/fred_api_v1.go
@@ -1,7 +1,13 @@
 package lib
 
+import "errors"
+
+const requestUrl = "https://api.stlouisfed.org/fred"
+
 type FredClient struct {
-	API_KEY    string
+	aPI_KEY    string
+	fileType   string
+	requestUrl string
 	Categories Category
 	Releases   Releases
 	Series     Series
@@ -9,10 +15,33 @@ type FredClient struct {
 	Tags       Tags
 }
 
-func CreateClient(ApiKey string) FredClient {
+func CreateClient(ApiKey string) (*FredClient, error) {
 
-	return FredClient{
-		API_KEY: ApiKey,
+	if ApiKey == "" {
+		return nil, errors.New("Operation may not be performed without an APIKEY. APIKEY's can be retrieved at your research.stlouisfed.org user account.")
 	}
 
+	return &FredClient{
+		aPI_KEY:    ApiKey,
+		fileType:   "json",
+		requestUrl: requestUrl + "api_key",
+	}, nil
+
+}
+
+func (f *FredClient) CallAPI() error {
+
+	if err := f.validateAPIKEY(); err != nil {
+
+		return err
+
+	}
+	return nil
+}
+
+func (f *FredClient) validateAPIKEY() error {
+	if f.aPI_KEY == "" {
+		return errors.New("Operation may not be performed without an APIKEY. APIKEY's can be retrieved at your research.stlouisfed.org user account.")
+	}
+	return nil
 }
