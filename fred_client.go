704fc2a15c8c18ba47b6ff2a8613e8d8403dadc6
Nick Sweko <nicholas_swekosky@yahoo.com>
2017-01-16 20:52:12 -0600

    master Updatd summary file for fred go toolkit

--- fred_client.go
+++
@@ -1,4 +0,0 @@
-/*
-Fred_Go_Toolkit a GoLang toolkit for retrieving the Federal Reserve Economic Data. FRED contains frequently updated US macro and regional economic time series at annual, quarterly, monthly, weekly, and daily frequencies. FRED aggregates economic data from a variety of sources - most of which are US government agencies. The economic time series in FRED contains observation or measurement periods associated with data values. For instance, the US unemployment rate for the month of January, 1990 was 5.4 percent and for the month of January, 2000 was 4.0 percent.
-*/
-package fred_client
